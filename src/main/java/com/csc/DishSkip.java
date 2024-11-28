3e2687570401a1c50c14b35b8cdd5c23d9a18353
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-11-28 01:12:23 -0600

    complete

--- src/main/java/com/csc/DishSkip.java
+++ src/main/java/com/csc/DishSkip.java
@@ -1,6 +1,55 @@
 package com.csc;
 
+class Dish {
+    public String description;
+
+    Dish(String description) {
+        this.description = description;
+    }
+}
+
 public class DishSkip {
+    private Dish[] stack;
+    private int top;
+    private int maxSize;
 
+    public DishSkip(int maxSize) {
+        this.maxSize = maxSize;
+        this.stack = new Dish[maxSize];
+        this.top = -1;
+    }
 
+    public void push(Dish dish) {
+        if (top == maxSize - 1) {
+            System.out.println("The stack is full. Cannot add more dishes.");
+        } else {
+            stack[++top] = dish;
+        }
+    }
+
+    public Dish pop() {
+        if (isEmpty()) {
+            System.out.println("The stack is empty. Cannot pop any dishes.");
+            return null;
+        } else {
+            return stack[top--];
+        }
+    }
+
+    public Dish peek() {
+        if (isEmpty()) {
+            System.out.println("The stack is empty. Nothing to peek.");
+            return null;
+        } else {
+            return stack[top];
+        }
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    private boolean isEmpty() {
+        return top == -1;
+    }
 }
--- src/test/java/com/csc/TestDishSkip.java
+++ src/test/java/com/csc/TestDishSkip.java
@@ -1,24 +1,76 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 
 public class TestDishSkip {
 
-  DishSkip skip;
+    @Test
+    public void testPushAndSize() {
+        DishSkip stack = new DishSkip(3);
+        Dish dish1 = new Dish("Dish 1");
+        Dish dish2 = new Dish("Dish 2");
 
-  @BeforeEach
-  void setUp() {
-    skip = new DishSkip();
-  }
+        stack.push(dish1);
+        stack.push(dish2);
 
-  // Put your tests here!
+        assertEquals(2, stack.size());
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    public void testPop() {
+        DishSkip stack = new DishSkip(3);
+        Dish dish1 = new Dish("Dish 1");
+        Dish dish2 = new Dish("Dish 2");
 
+        stack.push(dish1);
+        stack.push(dish2);
+
+        Dish poppedDish = stack.pop();
+        assertEquals("Dish 2", poppedDish.description);
+        assertEquals(1, stack.size());
+    }
+
+    @Test
+    public void testPeek() {
+        DishSkip stack = new DishSkip(3);
+        Dish dish1 = new Dish("Dish 1");
+        Dish dish2 = new Dish("Dish 2");
+
+        stack.push(dish1);
+        stack.push(dish2);
+
+        Dish peekedDish = stack.peek();
+        assertEquals("Dish 2", peekedDish.description);
+        assertEquals(2, stack.size());
+    }
+
+    @Test
+    public void testPushBeyondCapacity() {
+        DishSkip stack = new DishSkip(2);
+        Dish dish1 = new Dish("Dish 1");
+        Dish dish2 = new Dish("Dish 2");
+        Dish dish3 = new Dish("Dish 3");
+
+        stack.push(dish1);
+        stack.push(dish2);
+        stack.push(dish3);
+
+        assertEquals(2, stack.size());
+    }
+
+    @Test
+    public void testPopFromEmptyStack() {
+        DishSkip stack = new DishSkip(3);
+        Dish poppedDish = stack.pop();
+        assertNull(poppedDish);
+    }
+
+    @Test
+    public void testPeekFromEmptyStack() {
+        DishSkip stack = new DishSkip(3);
+        Dish peekedDish = stack.peek();
+        assertNull(peekedDish);
+    }
 }
